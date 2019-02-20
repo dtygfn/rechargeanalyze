a1b8318486cf9d5f57079b62372ba4311ff65534
dtygfn <1214948483@qq.com>
2019-02-20 09:37:48 +0800

    设置了项目中用到的常量

---
+++ src/main/java/constants/Constants.java
@@ -0,0 +1,27 @@
+package constants;
+
+/**
+ * 项目中用到的常量类文件
+ */
+public interface Constants {
+    /**
+     * 项目中相关的常量
+     */
+    String JDBC_DRIVER = "jdbc.driver";
+    String JDBC_DATASOURCE_SIZE = "jdbc.datasource.size";
+    String JDBC_URL = "jdbc.url";
+    String JDBC_USER = "jdbc.user";
+    String JDBC_PASSWORD = "jdbc.password";
+    String JDBC_URL_PROD = "jdbc.url.prod";
+    String JDBC_USER_PROD = "jdbc.user.prod";
+    String JDBC_PASSWORD_PROD = "jdbc.password.prod";
+    String SPARK_LOCAL = "spark.local";
+    String SPARK_LOCAL_TASKID_SESSION = "spark.local.taskid.session";
+    String SPARK_LOCAL_TASKID_PAGE = "spark.local.taskid.page";
+    String SPARK_LOCAL_TASKID_PRODUCT = "spark.local.taskid.product";
+    String KAFKA_METADATA_BROKER_LIST = "kafka.metadata.broker.list";
+    String KAFKA_TOPICS = "kafka.topics";
+    String CMCC_PATH = "cmccPath";
+    String GROUPID = "groupid";
+
+}
